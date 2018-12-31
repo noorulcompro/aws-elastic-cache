cad7a1d3c8c3b85458cd732ce69eedce320fe685
Ankur Mishra <ankur.mishra@comprotechnologies.com>
2018-12-31 17:39:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -11,50 +11,12 @@ var options = {
 };
 console.log('Creating Redis Client');
 var client = redis.createClient(options.port, options.host, function() {
-  console.log('222222222');
-  var data = [ 's:anal:dev1:ingestion:yoyo' ];
-  if(true) { data.push('classId'); }
-  else { data.push('standard'); }
-  client.multi()
-  .sadd(data)
-  .expire(data[0], 15*60)
-  .exec(function(err, res) {
-    if(err) {
-      console.log(err);
-    }
-    else { console.log(res); }
-  });
-});
+    console.log('222222222');
   
-
-
-
-
-
-
-
-/*client.auth(options.password, function (err) {
-  if (err) { throw err; }
-  else {
-    client.sadd(['key', 'd', 'k'], function(err, res) {
-      if(err) { console.log(err); }
-      else {
-        console.log((res));
-        _und.each(res, function(value, key) {
-          k++;
-          classrecord[key] = value;
-          
-        })
-        console.log(k);
-        var a = JSON.stringify(classrecord, null, 4);
-        fs.writeFile('/test.txt', a, function(err) {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          //file written successfully
-        })
-      }
-    });
-  }
-});*/
+    client.set("foo_rand000000000000", "OK");
+ 
+    // This will return a JavaScript String
+    client.get("foo_rand000000000000", function (err, reply) {
+    console.log(reply.toString()); // Will print `OK`
+});
+});
