7893d8bce3c6c24ae74808f85afc3518a606f269
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2018-12-31 18:03:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -16,9 +16,10 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
   
-client.hset("foo_rand000000000000", "key", "value", function(a, b) { console.log(a); console.log(b); });
+client.hset("redis-key-2", "key", "value", function(a, b) { console.log(a); console.log(b); });
  
 // This will return a JavaScript String
-client.get("foo_rand000000000000", function (err, reply) {
+client.get("redis-key-2", function (err, reply) {
+  console.log('Getting Redis Key');
   console.log(reply.toString()); // Will print `OK`
 });
