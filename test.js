fa44b518dc0ddbdd504baea5a29af57dd24cf8f0
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-01-02 17:22:03 +0530

    Update test.js
    primary node url

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 //var _und = require('underscore');
 var options = {
-  "host": "test-redis-replica.yaiwig.ng.0001.use1.cache.amazonaws.com",
+  "host": "test-redis-replica-001.yaiwig.0001.use1.cache.amazonaws.com",
   "port": 6379
 };
 console.log('Creating Redis Client');
