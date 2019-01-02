a1bcebca1a6bd89ab33736d7e0c8879db0d73acb
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-02 15:56:08 +0530

    Create testExpire.js

---
+++ testExpire.js
@@ -0,0 +1,21 @@
+var redis = require("redis");
+
+//var _und = require('underscore');
+var options = {
+  "host": "test-redis.yaiwig.0001.use1.cache.amazonaws.com",
+  "port": 6379
+};
+console.log('Creating Redis Client');
+var client = redis.createClient(options.port, options.host);
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+var redisKey = Math.floor((Math.random() * 10000000) + 1);
+console.log(redisKey);
+client.setex(redisKey, "noor", "ali", 30 function(res, err) {
+  console.log('res');
+  console.log(res);
+  console.log('err');
+  console.log(err);
+});
