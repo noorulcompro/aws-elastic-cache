3c445780a37f484a14b16a20fd9249561afc2633
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-03 11:40:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -17,16 +17,18 @@ client.on("error", function (err) {
 });
 var redisKey = 'a-' + Math.floor((Math.random() * 10000000) + 1);
 console.log(redisKey);
-client.hset(redisKey, "name", "redis-test", function(a, b) {
-  console.log('a');
-  console.log(a);
-  console.log('b');
-  console.log(b);
+client.hset(redisKey, "name", "redis-test", function(err, res) {
+  console.log('err');
+  console.log(err);
+  console.log('res');
+  console.log(res);
 });
  
 // This will return a JavaScript String
-client.hgetall(redisKey, function (err, reply) {
+client.hgetall(redisKey, function (err, res) {
   console.log('Getting Redis Key');
+  console.log('err');
   console.log(err);
-  console.log(reply);
+  console.log('res');
+  console.log(res);
 });
