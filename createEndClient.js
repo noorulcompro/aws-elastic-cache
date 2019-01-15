dc38a816eae803adcf835407f58feac18299fabe
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-01-15 18:01:12 +0530

    Create createEndClient.js

---
+++ createEndClient.js
@@ -0,0 +1,39 @@
+var redis = require("redis");
+var timeout = 1000; // = 1 sec, (in miliseconds)
+var bConnect = false, bReady = false;
+
+var options = {
+    'host': 'thor-transactions.dwnzoe.ng.0001.usw2.cache.amazonaws.com',
+    'port': 6379
+};
+
+function createClient() {
+  setTimeout(function() {
+    var client = redis.createClient(options.port, options.host);
+  }, timeout);
+}
+
+client.on('connect', function () {
+    console.log('Redis client connected');
+    if(bConnect && bReady) {
+      client.end(true);
+    }
+});
+
+client.on('ready', function () {
+    console.log('Redis client ready');
+    if(bConnect && bReady) {
+      client.end(true);
+    }
+});
+
+client.on('end', function () {
+    console.log('Redis client ended');
+    createClient();
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+createClient();
