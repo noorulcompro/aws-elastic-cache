ddbae06745737188281a9340d226d4831a65b3ad
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-16 19:01:52 +0530

    Update getAllRedisKeysThenAllReisValuesofEachKey.js

--- getAllRedisKeysThenAllReisValuesofEachKey.js
+++ getAllRedisKeysThenAllReisValuesofEachKey.js
@@ -39,7 +39,7 @@ function createClient() {
           console.log('No Key exists')
         }
         else {
-           for(var i =0 ; i<500; i++) {
+           for(var i =0 ; i<5; i++) {
                configKeys = configKeys.concat(configKeys);
            }
             count++;
