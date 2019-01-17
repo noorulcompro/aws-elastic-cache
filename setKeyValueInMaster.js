7b5ce7f1b1b364a42e2312419a800da71e6772dc
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-17 11:33:52 +0530

    Create setKeyValueInMaster.js

---
+++ setKeyValueInMaster.js
@@ -0,0 +1,30 @@
+var redis = require("redis");
+var fs = require('fs');
+
+var options = {
+  "host": "thor-analytics-001.dwnzoe.0001.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+console.log('Creating Redis Client');
+var client = redis.createClient(options.port, options.host);
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.on('connect', function() {
+  console.log('Redis connect event');
+});
+ 
+// This will return a JavaScript String
+client.set('Anshika', 'Srivastava', function(err, response) {
+  var setTime = Date.now();
+  console.log('setTime' + setTime);
+  if(err) {
+    console.log('err' + err);
+  }
+  else {
+    console.log('response' + response);
+  }
+});
