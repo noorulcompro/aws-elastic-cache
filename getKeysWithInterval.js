2dd5f1ce1f4d0c532583366cd26d7e5432197d3e
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-17 13:51:01 +0530

    Update getKeysWithInterval.js

--- getKeysWithInterval.js
+++ getKeysWithInterval.js
@@ -1,9 +1,9 @@
 var redis = require("redis");
-var timeout = 5; // = 1 sec, (in miliseconds)
+var timeout = 500; // = 1 sec, (in miliseconds)
 var counter = 0;
 
 var options = {
-    'host': 'thor-transactions.dwnzoe.ng.0001.usw2.cache.amazonaws.com',
+    'host': 'thor-transactions-001.dwnzoe.0001.usw2.cache.amazonaws.com',
     'port': 6379
 };
 
@@ -13,29 +13,25 @@ client.on('connect', function () {
     console.log('Redis client connected');
 });
 
+client.on('ready', function () {
+    console.log('Redis client ready');
+});
+
 client.on('error', function (err) {
     console.log('Something went wrong ' + err);
 });
 
-client.set(['test-key-1', 'test-value-10101'], function (error, result) {
-    if (error) {
-        console.log(error);
-        throw error;
-    }
-    console.log('SET(only once) key --- ' + result);
-});
-
-
-console.log('GETting value--key - test-key-1');
+var redisKey = 's:ballu:anupam';
+console.log('GETting key - ' + redisKey);
 setInterval(function() {
     counter++;
-    client.get('test-key-1', function (error, result) {
+    client.get(redisKey, function (error, result) {
         if (error) {
           console.log("ERROR at getting-----");
           console.log(error);
         }
         else {
-            if(counter % 5000 == 0) { console.log('Running Succesfully...'); }
+            if(counter % 2 == 0) { console.log('Running Succesfully...'); }
         }
     });
 }, timeout);
