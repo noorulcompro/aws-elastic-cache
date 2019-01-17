52ce526921e70c4e9ff3ad4a53684e5924bbc596
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-17 12:27:09 +0530

    Update getKeyValueFromReplica.js

--- getKeyValueFromReplica.js
+++ getKeyValueFromReplica.js
@@ -19,12 +19,12 @@ client.on('connect', function() {
  
 // This will return a JavaScript String
 var id = setInterval(function() {
-  client.get('190001', function(err, response) {
+  client.get('290001', function(err, response) {
     if(err) {
       console.log('err' + err);
     }
     else {
-      if(response === '190002') {
+      if(response === '290002') {
         var getTimeOnCorrectResult = Date.now();
         console.log('getTimeOnCorrectResult ' + getTimeOnCorrectResult);
         clearInterval(id);
