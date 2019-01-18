2d1f9e1150861957a9362a3bbaaf73eeaf9d9a9d
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-01-18 13:07:01 +0530

    Update createMultiClients.js

--- createMultiClients.js
+++ createMultiClients.js
@@ -6,25 +6,29 @@ var options = {
   "port": 6379
 };
 
-var arrClients = [];
+var counter = 0;
 
 console.log('Creating Multiple Redis Clients');
 
 var id = setInterval(function() {
-  for(var i = 0; i<10; i++) {
+  for(var i = 0; i<20; i++) {
     var client = redis.createClient(options.port, options.host);
     client.on("error", function (err) {
       console.log("Error " + err);
     });
 
     client.on('connect', function() {
+        counter++;
 //      console.log('Redis connect event');
     });
 
     client.on('ready', function() {
 //      console.log('Redis ready event');
     });
-    arrClients.push(client);
+    
+    client.on('end', function() {
+      console.log('Redis end event');
+    });
   }
-  console.log('clients created : ' + arrClients.length);
+  console.log('clients created : ' + counter);
  }, 100);
