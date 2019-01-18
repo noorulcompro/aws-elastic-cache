c252ecb81109fcbb3136ac558844047e351bfa32
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-01-18 12:58:49 +0530

    Create createMultiClients.js

---
+++ createMultiClients.js
@@ -0,0 +1,30 @@
+var redis = require("redis");
+var fs = require('fs');
+
+var options = {
+  "host": "test-redis.dwnzoe.ng.0001.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+var arrClients = [];
+
+console.log('Creating Multiple Redis Clients');
+
+var id = setInterval(function() {
+  for(var i = 0; i<10; i++) {
+    var client = redis.createClient(options.port, options.host);
+    client.on("error", function (err) {
+      console.log("Error " + err);
+    });
+
+    client.on('connect', function() {
+      console.log('Redis connect event');
+    });
+
+    client.on('ready', function() {
+      console.log('Redis ready event');
+    });
+    arrClients.push(client);
+  }
+  console.log('clients created : ' + arrClients.length);
+ }, 1000);
