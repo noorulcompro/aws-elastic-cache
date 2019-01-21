a5ab0a4f3764227be9b2645d6ccd792612df6607
noorulcompro <37929763+noorulcompro@users.noreply.github.com>
2019-01-21 16:20:17 +0530

    Update failover-test.js

--- failover-test.js
+++ failover-test.js
@@ -10,17 +10,8 @@ var options = {
             // a individual error
             return new Error('The server refused the connection');
         }
-        if (options.total_retry_time > 1000 * 60 * 60) {
-            // End reconnecting after a specific timeout and flush all commands
-            // with a individual error
-            return new Error('Retry time exhausted');
-        }
-        if (options.attempt > 10) {
-            // End reconnecting with built in error
-            return undefined;
-        }
         // reconnect after
-        return Math.min(options.attempt * 100, 3000);
+        return 100;
     }
 };
 
