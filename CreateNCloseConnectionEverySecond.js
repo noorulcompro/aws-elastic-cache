388372faf5add39f80046254e329b107277f67ef
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-01-21 15:26:56 +0530

    Create CreateNCloseConnectionEverySecond.js

---
+++ CreateNCloseConnectionEverySecond.js
@@ -0,0 +1,35 @@
+var redis = require("redis");
+var fs = require('fs');
+
+var options = {
+  "host": "test-redis.dwnzoe.ng.0001.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+var counter = 0, loopCounter = 0;
+
+console.log('Creating Redis Clients Every milli Second');
+
+var id = setInterval(function() {
+  var client = redis.createClient(options.port, options.host);
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
+  client.on('connect', function() {
+    console.log('Redis connect event');
+    counter++;
+    console.log('clients created : ' + counter);
+  });
+
+  client.on('ready', function() {
+    console.log('Redis ready event');
+    console.log('Quitting Connection');
+    client.quit();
+    counter--;
+  });
+
+  client.on('end', function() {
+    console.log('Redis end event');
+  });
+ }, 1);
